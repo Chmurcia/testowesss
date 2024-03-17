518d635e3faae8cd9a493d4fca43ebfd46e4979a
Igor Grabowski <dogodontexist@gmail.com>
2024-03-17 17:24:28 +0100

    UwU

---
+++ test.js
@@ -0,0 +1,13 @@
+var number = 30;
+function CheckG30(num) {
+    if (num > 30) {
+        console.log('Greater!');
+    }
+    else if (num < 30) {
+        console.log('Lower!');
+    }
+    else {
+        console.log('Is equal!');
+    }
+}
+CheckG30(number);
